990a88d1eb95676ae394a2de5db7552fb7145b8a
Viviana <vivianafb15@gmail.com>
2021-11-02 15:59:06 -0300

    args

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import Server from './services/server';
-import Config from './config';
+import server from './services/server';
 import { connectToDB } from './services/dbMongo';
+import cluster from 'cluster';
+import { Argumentos } from './middleware/args';
+import os from 'os';
+const clusterMode = Argumentos;
+const puerto = process.env.PORT || 8080;
+connectToDB();
+// connectToDB().then(() =>{
+//     console.log('DB MONGO CONECTADA');
+//     server.listen(puerto, ()=> console.log(`Server up puerto ${puerto}`));
+// });
 
-const puerto = Config.PORT || 8080;
+const numCPUs = os.cpus().length;
+if (clusterMode && cluster.isMaster) {
+//   console.log(`NUMERO DE CPUS ===> ${numCPUs}`);
+//   console.log(`PID MASTER ${process.pid}`);
 
-connectToDB().then(() =>{
-    console.log('DB MONGO CONECTADA');
-    Server.listen(puerto, ()=> console.log(`Server up puerto ${puerto}`));
-});
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died at ${Date()}`);
+    cluster.fork();
+  });
+} else {
+  server.listen(puerto, () =>
+    console.log(
+      `Servidor express escuchando en el puerto ${puerto} - PID WORKER ${process.pid}`
+    )
+  );
+  server.on('error', error => console.log(`Error en el servidor: ${error}`));
+}
---
+++ src/middleware/args.js
@@ -0,0 +1,8 @@
+import minimist from 'minimist';
+import dotenv from 'dotenv';
+dotenv.config();
+const args = minimist(process.argv.slice(2));
+
+
+export const Argumentos = args;
+export const portArg = args.puerto || 8080;
