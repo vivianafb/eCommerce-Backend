8f62a0d95495197f0929540a51fbeb289e8aed12
= <=>
2022-01-04 11:29:41 -0300

    Arreglos finales

--- src/services/socket.js
+++ src/services/socket.js
@@ -16,74 +16,76 @@ export const initWsServer = (server) => {
   io.on("connection", async (socket) => {
     logger.warn("CONEXION ESTABLECIDA WEBSOCKET");
 
-
     socket.on("newMessage", async (mensajes) => {
       const username = mensajes.usuario;
-      
+
       const user = await UserAPI.query(username);
+      if (user) {
+        if (mensajes.mensajes.toLowerCase().includes("stock")) {
+          const productos = await productsAPI.getProducts();
+          let pro = [];
+          let stock = [];
+          let nombre = [];
+          for (let i = 0; i < productos.length; i++) {
+            nombre = productos[i].nombre;
+            stock = productos[i].stock;
+            pro.push({ nombre: nombre, stock: stock });
+          }
 
-      if (mensajes.mensajes.toLowerCase().includes("stock")) {
-        const productos = await productsAPI.getProducts();
-        let pro = [];
-        let stock = [];
-        let nombre = [];
-        for (let i = 0; i < productos.length; i++) {
-          nombre = productos[i].nombre;
-          stock = productos[i].stock;
-          pro.push({ nombre: nombre, stock: stock });
+          console.log(`stock : ${pro}`);
+          let data = {
+            userId: user._id,
+            usuario: mensajes.usuario,
+            mensajeUser: mensajes.mensajes,
+            mensajeBot: pro,
+          };
+          MensajesController.addMensaje(data);
+          io.emit("newMessage", data);
+        } else if (mensajes.mensajes.toLowerCase().includes("orden")) {
+          const orden = await orderApi.getOrder(user._id);
+
+          let order = [];
+          let items = JSON.stringify(orden[orden.length - 1].items);
+          let total = orden[orden.length - 1].total;
+          order.push({ items: items, total: total });
+
+          let data = {
+            userId: user._id,
+            usuario: mensajes.usuario,
+            mensajeUser: mensajes.mensajes,
+            mensajeBot: order,
+          };
+
+          MensajesController.addMensaje(data);
+          io.emit("newMessage", data);
+        } else if (mensajes.mensajes.toLowerCase().includes("carrito")) {
+          const carrito = await carritoAPI.getCarrito(user._id);
+          let cart = [];
+          let productos = carrito.productos;
+          cart.push({ productos: productos });
+          for (let i = 0; i < carrito.length; i++) {}
+          let data = {
+            userId: user._id,
+            usuario: mensajes.usuario,
+            mensajeUser: mensajes.mensajes,
+            mensajeBot: carrito,
+          };
+          console.log(`CARRITO :${cart}`);
+          MensajesController.addMensaje(data);
+          io.emit("newMessage", data);
+        } else {
+          const message = `Hola! No he podido comprender tu mensaje. Por favor ingresa una de las siguientes opciones "STOCK": Para conocer nuestro stock actual.  "ORDEN" :Para conocer la informacion de tu ultima orden "CARRITO" :Para concer el estado actual de tu carrito`;
+          let data = {
+            userId: user._id,
+            usuario: mensajes.usuario,
+            mensajeUser: mensajes.mensajes,
+            mensajeBot: message,
+          };
+          MensajesController.addMensaje(data);
+          io.emit("newMessage", data);
         }
-
-        console.log(`stock : ${pro}`);
-        let data = {
-          userId: user._id,
-          usuario: mensajes.usuario,
-          mensajeUser: mensajes.mensajes,
-          mensajeBot: pro,
-        };
-        MensajesController.addMensaje(data);
-        io.emit("newMessage", data);
-      } else if (mensajes.mensajes.toLowerCase().includes("orden")) {
-        const orden = await orderApi.getOrder(user._id);
-
-        let order = [];
-        let items = JSON.stringify(orden[orden.length - 1].items);
-        let total = orden[orden.length - 1].total;
-        order.push({ items: items, total: total });
-
-        let data = {
-          userId: user._id,
-          usuario: mensajes.usuario,
-          mensajeUser: mensajes.mensajes,
-          mensajeBot: order,
-        };
-
-        MensajesController.addMensaje(data);
-        io.emit("newMessage", data);
-      } else if (mensajes.mensajes.toLowerCase().includes("carrito")) {
-        const carrito = await carritoAPI.getCarrito(user._id);
-        let cart = [];
-        let productos = carrito.productos;
-        cart.push({ productos: productos });
-        for (let i = 0; i < carrito.length; i++) {}
-        let data = {
-          userId: user._id,
-          usuario: mensajes.usuario,
-          mensajeUser: mensajes.mensajes,
-          mensajeBot: carrito,
-        };
-        console.log(`CARRITO :${cart}`);
-        MensajesController.addMensaje(data);
-        io.emit("newMessage", data);
       } else {
-        const message = `Hola! No he podido comprender tu mensaje. Por favor ingresa una de las siguientes opciones "STOCK": Para conocer nuestro stock actual.  "ORDEN" :Para conocer la informacion de tu ultima orden "CARRITO" :Para concer el estado actual de tu carrito`;
-        let data = {
-          userId: user._id,
-          usuario: mensajes.usuario,
-          mensajeUser: mensajes.mensajes,
-          mensajeBot: message,
-        };
-        MensajesController.addMensaje(data);
-        io.emit("newMessage", data);
+        logger.warn("EL USUARIO NO EXISTE");
       }
     });
   });
